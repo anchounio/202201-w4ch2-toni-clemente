5c5c4ba3e73f03857b5427d5e1742486839efb68
anchounio <toni.clemente.garcia@gmail.com>
2022-02-08 19:14:52 +0100

    Visible gentlemen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Info } from './components/info';
+import { Gentleman } from './components/gentleman';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Info></Info>
+            <main class="main">
+                <Gentleman></Gentleman>
+            </main>
+        </div>
+    );
 }
 
 export default App;
